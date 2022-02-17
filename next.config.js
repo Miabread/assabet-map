f64e5cacdff3972ff6ec9b32c705b16141a344b7
JamesBeeProg <JamesBeeProg@gmail.com>
2022-02-17 08:03:49 -0500

    Disable pwa in dev

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const withPWA = require('next-pwa');
+const prod = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
     reactStrictMode: true,
     pwa: {
+        disable: !prod,
         dest: 'public',
     },
 });
